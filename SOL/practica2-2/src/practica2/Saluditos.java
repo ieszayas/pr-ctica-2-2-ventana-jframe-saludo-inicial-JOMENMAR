b44706109b5c626cf28f87a76dee7448f567abf4
JOMENMAR <160268639+JOMENMAR@users.noreply.github.com>
2024-10-01 17:29:08 +0200

    V4
    Perfeccionado

--- SOL/practica2-2/src/practica2/Saluditos.java
+++ SOL/practica2-2/src/practica2/Saluditos.java
@@ -1,3 +1,5 @@
+package practica2;
+
 
 import javax.swing.*;
 
@@ -10,12 +12,12 @@ import javax.swing.*;
  *
  * @author Jorge
  */
-public class NewJFrame extends javax.swing.JFrame {
+public class Saluditos extends javax.swing.JFrame {
 
     /**
      * Creates new form NewJFrame
      */
-    public NewJFrame() {
+    public Saluditos() {
         
         initComponents();
         setLocationRelativeTo(null); // Centrar en la pantalla
@@ -32,84 +34,99 @@ public class NewJFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         Mano = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
+        Texto_Comunicacion = new javax.swing.JLabel();
+        Boton_Confirma_Saludo = new javax.swing.JButton();
+        Introducir_nombre_completo = new javax.swing.JTextField();
+        Imagen_mano = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Saludo_al_usuario");
 
         Mano.setIcon(new javax.swing.ImageIcon(getClass().getResource("/practica2/pkg2/media/saluda.png"))); // NOI18N
 
-        jLabel1.setText("Escribe el nombre del usuario:");
+        Texto_Comunicacion.setText("Escribe el nombre del usuario:");
 
-        jButton1.setText("Saludo");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        Boton_Confirma_Saludo.setText("Saludo");
+        Boton_Confirma_Saludo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                Boton_Confirma_SaludoActionPerformed(evt);
             }
         });
 
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+        Introducir_nombre_completo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+                Introducir_nombre_completoActionPerformed(evt);
             }
         });
+
+        Imagen_mano.setIcon(new javax.swing.ImageIcon(getClass().getResource("/media/saluda.png"))); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(178, 178, 178)
-                        .addComponent(Mano))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(108, 108, 108)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 197, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(95, Short.MAX_VALUE))
+                .addGap(178, 178, 178)
+                .addComponent(Imagen_mano)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(Mano)
+                .addContainerGap(166, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel1)
+                        .addComponent(Texto_Comunicacion)
                         .addGap(117, 117, 117))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jButton1)
-                        .addGap(163, 163, 163))))
+                        .addComponent(Boton_Confirma_Saludo)
+                        .addGap(161, 161, 161))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(Introducir_nombre_completo, javax.swing.GroupLayout.PREFERRED_SIZE, 197, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(94, 94, 94))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(48, 48, 48)
-                .addComponent(Mano)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(48, 48, 48)
+                        .addComponent(Mano)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap(29, Short.MAX_VALUE)
+                        .addComponent(Imagen_mano)
+                        .addGap(18, 18, 18)))
+                .addComponent(Texto_Comunicacion)
                 .addGap(18, 18, 18)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 76, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(35, 35, 35))
+                .addComponent(Introducir_nombre_completo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28)
+                .addComponent(Boton_Confirma_Saludo)
+                .addContainerGap(46, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        String nombreCompleto = jTextField1.getText().trim();
+    private void Boton_Confirma_SaludoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Boton_Confirma_SaludoActionPerformed
+        String nombreCompleto = Introducir_nombre_completo.getText().trim();
 
         // Validaciones
         if (nombreCompleto.isEmpty()) {
             JOptionPane.showMessageDialog(this, "Por favor, rellena el campo.", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
         }
+        
         if (nombreCompleto.split(" ").length < 2) {
             JOptionPane.showMessageDialog(this, "Por favor, introduce al menos un nombre y un apellido.", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
         }
+        
         if (nombreCompleto.length() < 5) {
             JOptionPane.showMessageDialog(this, "El nombre y apellidos deben tener al menos 5 caracteres en total.", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
         }
-        if (!nombreCompleto.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ]+")) {
+        if (!nombreCompleto.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ //s]+")) {
             // Mensaje de error si hay números o símbolos
             JOptionPane.showMessageDialog(this, "El nombre no debe contener números ni símbolos.", "Error", JOptionPane.ERROR_MESSAGE);
         } else {
@@ -117,13 +134,13 @@ public class NewJFrame extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(this, "Hola " + nombreCompleto + "!", "Saludo", JOptionPane.INFORMATION_MESSAGE);
 
             // Borrar el campo de texto
-            jTextField1.setText("");
+            Introducir_nombre_completo.setText("");
         }
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_Boton_Confirma_SaludoActionPerformed
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+    private void Introducir_nombre_completoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Introducir_nombre_completoActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    }//GEN-LAST:event_Introducir_nombre_completoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -142,28 +159,30 @@ public class NewJFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Saluditos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Saluditos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Saluditos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Saluditos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame().setVisible(true);
+                new Saluditos().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Boton_Confirma_Saludo;
+    private javax.swing.JLabel Imagen_mano;
+    private javax.swing.JTextField Introducir_nombre_completo;
     private javax.swing.JLabel Mano;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JLabel Texto_Comunicacion;
     // End of variables declaration//GEN-END:variables
 }
